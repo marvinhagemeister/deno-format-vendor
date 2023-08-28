2e85826d7592ae65a7ff05c44663d1b784e5381d
Marvin Hagemeister <marvin@deno.com>
2023-08-28 12:28:49 +0200

    Add bug repro

---
+++ foo.ts
@@ -0,0 +1,2 @@
+import * as twindCore from "https://esm.sh/@twind/core@1.1.3";
+console.log(typeof twindCore.install === "function");
---
+++ vendor/esm.sh/@twind/#core@1.1.3_ea8ce.js
@@ -0,0 +1,2 @@
+/* esm.sh - @twind/core@1.1.3 */
+export * from "https://esm.sh/v124/@twind/core@1.1.3/deno/core.mjs";
